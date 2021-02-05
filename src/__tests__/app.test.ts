0bd6bb67580d839004a2419b9ff14e69c7b64b49
Xun Zhou <segentor@gmail.com>
2021-02-05 16:40:57 +0100

    updates

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/svelte";
 import App from "../App.svelte";
 
-test("should render", () => {
+test("should render the text hello world!", () => {
   const results = render(App, { props: { name: "world" } });
 
   expect(() => results.getByText("Hello world!")).not.toThrow();
